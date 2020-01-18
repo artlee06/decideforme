5880fa274a8bd0b8ae821c4ea3d6c30ed3fdccf7
Arthur Lee <ykarthurlee@gmail.com>
2020-01-19 02:25:26 +0800

    Added Skeleton

---
+++ src/components/SnekPage.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+
+// Material UI
+// import { makeStyles } from "@material-ui/core/styles";
+import { createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/styles";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import Button from '@material-ui/core/Button';
+import Box from "@material-ui/core/Box";
+
+import { useHistory } from "react-router-dom";
+
+export default function SnekPage() {
+  let history = useHistory();
+
+  return (
+    <React.Fragment>
+      <div>
+        <Paper elevation={0} square>
+          <Box>
+            <Typography variant="h2">
+                SnekMode
+            </Typography>
+          </Box>
+        </Paper>
+      </div>
+    </React.Fragment>
+  );
+}
+
